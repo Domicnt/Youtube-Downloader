a478b3fdf3155acbbd8b15d6e079f97507b1b4a0
Dominic Bertocchini <dominicbertocchini@gmail.com>
2022-01-12 13:30:09 -0800

    fixed issue with invalid video names

--- index.js
+++ index.js
@@ -3,14 +3,23 @@ const ytdl = require('ytdl-core');
 const ytpl = require('ytpl');
 
 function download (url, title) {
-    ytdl(url, {
-        format: 'mp3'
-    }).pipe(fs.createWriteStream('songs/'+title+'.mp3'));
-    console.log(title + " downloaded");
+    //check if title is valid and remove invalid characters
+    var str = "";
+    var lastCode = 0;
+    for (i = 0, len = title.length; i < len; i++) {
+        code = title.charCodeAt(i);
+        if (!(code == 32 && str.charCodeAt(str.length - 1) == 32) && (code > 31 && code < 128) && (code != 47 && code != 92)) {
+            str += String.fromCharCode(code);
+        }
+        lastCode = code;
+    }
+    
+    ytdl(url, {format: 'mp3'}).pipe(fs.createWriteStream('songs/'+str+'.mp3'));
+    console.log(title + " downloaded" + (str!=title?(", name changed to "+str):"") + ".");
 }
 
 var playlists = [
-    'PL1-8jr0CH4XENRRPD1m__bf7pz4AFNvfP'
+    'PL1-8jr0CH4XENRRPD1m__bf7pz4AFNvfP',
 ];
 
 async function downloadAll() {
