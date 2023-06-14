29dc03807f35ad443dad74dc45f4a67d94f19fab
Dominic Bertocchini <dominicbertocchini@gmail.com>
2023-06-13 17:52:09 -0700

    added config file

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
+const config = require('./config.json');
+
 const ytdl = require('ytdl-core');
 const ytpl = require('ytpl');
+
 //keep track of how many videos have no valid characters in their name
 var defaultNum = 1;
 
-function download (url, title) {
+async function download (url, title) {
     //check if title is valid and remove invalid characters
     var str = "";
     for (i = 0, len = title.length; i < len; i++) {
         code = title.charCodeAt(i);
-        if (!(code == 32 && str.charCodeAt(str.length - 1) == 32) && (code > 31 && code < 123) && (!(code > 32 && code < 48) || code == 39) && !(code > 57 && code < 65) && !(code > 90 && code < 97)) {
+        if (!(code == 32 && str.charCodeAt(str.length - 1) == 32) && //avoid 2 subsequent spaces
+        (code > 31 && code < 127) && //avoid non-printing characters
+        !([60, 62, 58, 34, 47, 124, 92, 63, 42].includes(code))) { //avoid illegal characters for windows filenames
             str += String.fromCharCode(code);
         }
     }
@@ -20,18 +25,25 @@ function download (url, title) {
         defaultNum++;
     }
 
-    ytdl(url, {filter: 'audioonly'}).pipe(fs.createWriteStream('songs/'+str+'.mp3'));
-    console.log(title + " downloading" + (str!=title?(", name changed to "+str):"") + ".");
+    let info = await ytdl.getInfo(url);
+    let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+    let audioFormat = ytdl.chooseFormat(audioFormats, {quality: 'highestaudio'});
+
+    let name = str+'.'+audioFormat.audioCodec;
+    let path = 'downloads/'+name;
+    if (!fs.existsSync(path)) {
+        let stream = ytdl(url, {format: audioFormat});
+        stream.pipe(fs.createWriteStream(path));
+        console.log(name + " downloading.");
+    }
 }
 
-var playlists = [
-    'PL1-8jr0CH4XENRRPD1m__bf7pz4AFNvfP',
-];
+var playlists = config.playlists;
 
 async function downloadAll() {
     for (const ID of playlists) {
         console.log('Playlist ID requested: ' + ID);
-        const playlist = await ytpl(ID, {limit: 9999});
+        const playlist = await ytpl(ID, {limit: Infinity});
         playlist.items.forEach(element => {
             download(element.url, element.title);
         });
